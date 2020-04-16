aa069385881335b2990b55e2f5a0eadeefed41ea
Magal97 <magal97@outlook.com>
2020-04-16 16:49:09 -0300

    backend

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public deleteTransactionWrongBalance(id: string) {
+    const repositoryIndex = this.transactions.findIndex(tran => tran.id === id);
+    this.transactions.splice(repositoryIndex, 1);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .map(transaction =>
+        transaction.type === 'income' ? transaction.value : 0,
+      )
+      .reduce((total, currentValue) => total + currentValue, 0);
+
+    const outcome = this.transactions
+      .map(transaction =>
+        transaction.type === 'outcome' ? transaction.value : 0,
+      )
+      .reduce((total, currentValue) => total + currentValue, 0);
+
+    const total = income - outcome;
+    return { total, income, outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Transaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ id, title, value, type }: Transaction): Transaction {
+    const transaction = this.transactionsRepository.create({
+      id,
+      title,
+      value,
+      type,
+    });
+
+    const { outcome, total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && outcome > total) {
+      this.transactionsRepository.deleteTransactionWrongBalance(id);
+      throw Error('Invalid balance');
+    }
+
+    return transaction;
   }
 }
 
